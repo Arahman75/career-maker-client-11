d38d846ea20578f5325bf34bc4c489d182cd7926
Arahman75 <selfarahman01825@gmail.com>
2023-11-04 20:44:14 +0600

    create assigntment 11 repository

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+
+  return (
+    <>
+
+
+      <h1 className='text-red-600'>Career Maker</h1>
+      <button className="btn btn-secondary">Secondary</button>
+
+    </>
+  )
+}
+
+export default App
